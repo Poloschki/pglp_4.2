f4fcb42ebdee72f8e3e68d4db6a97d481dd60d93
Poloschki <patrick77290@gmail.com>
2020-03-30 11:57:55 +0200

    Création de la class Quit qui implémente Command

---
+++ src/main/java/Quit.java
@@ -0,0 +1,13 @@
+public class Quit implements Command {
+    private Interpreter interpreter;
+
+    public Quit(Interpreter interpreter) {
+        this.interpreter = interpreter;
+    }
+
+    @Override
+    public void execute() {
+        this.interpreter.quit();
+    }
+
+}
