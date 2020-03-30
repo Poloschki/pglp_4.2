dff714c746a0a774b9366b2fd114116fd0f75641
Poloschki <patrick77290@gmail.com>
2020-03-30 13:03:44 +0200

    Implémentation de CommandQuit

--- src/main/java/CommandQuit.java
+++ src/main/java/CommandQuit.java
@@ -1,3 +1,12 @@
-public class CommandQuit {
+public class CommandQuit implements Command {
+    private Interpreter interpreter;
 
+    public CommandQuit(Interpreter interpreter) {
+        this.interpreter = interpreter;
+    }
+
+    @Override
+    public void execute() {
+        this.interpreter.quit();
+    }
 }
